866ad585dc19810b8c490c0453c5e5e82b8a1069
SanketBanait <2020ucp1984@mnit.ac.in>
2021-03-30 17:04:53 +0530

    otherchanges

--- 10PalindromeNumber.cpp
+++ 10PalindromeNumber.cpp
@@ -2,23 +2,23 @@
 using namespace std;
 int main()
 {
- int num_copy, num, digit, rev_num = 0;
+ int num, num, digit, rev = 0;
 
  cout << "Enter a positive number: ";
  cin >> num;
 
- num_copy = num;
+ num = num;
 
  do
  {
  digit = num % 10;
- rev_num = (rev_num * 10) + digit;
+ rev = (rev * 10) + digit;
  num = num / 10;
  }while(num != 0);
 
- cout <<"The reverse of the number is: "<<rev_num<<endl;
+ cout <<"The reverse of the number is: "<<rev<<endl;
 
- if (num_copy == rev_num) cout << " The number is a palindrome";
+ if (num == rev) cout << " The number is a palindrome";
  else cout << " The number is not a palindrome";
 
  return 0;
